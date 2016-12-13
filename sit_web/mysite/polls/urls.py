dfcca86efba7f94299fb469db25e3851dc8771e0
stefanoprea <stefanoprea128@gmail.com>
2016-12-13 23:23:17 +0200

    Update urls.py

--- sit_web/mysite/polls/urls.py
+++ sit_web/mysite/polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
  # ex: /polls/
- url(r'^$', views.index, name='index'),
+ url(r'^.*$', views.index, name='index'),
  # ex: /polls/5/
  #url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
  # ex: /polls/5/results/
