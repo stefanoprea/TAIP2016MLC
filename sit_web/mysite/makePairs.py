8b850b95887b2524dea3440b3b14eec8a79f4b9e
stefanoprea <stefanoprea128@gmail.com>
2016-12-14 18:34:29 +0200

    some encodings

--- sit_web/mysite/makePairs.py
+++ sit_web/mysite/makePairs.py
@@ -11,30 +11,36 @@ except:
     def isNoun(t):
         return os.system("wordnet "+t+" -synsn >/dev/null")!=0
 
-def randword(f,length):
+def randword(f,length,enc):
     pos=randint(0,length-1)
     f.seek(pos)
     t=f.read(1)
-    while t!="" and not t.isspace():
+    while len(t)!=0 and ord(t)!=10:
         t=f.read(1)
-    t=f.readline().strip().strip(punctuation)
-    if t!="" and isNoun(t): #numai substantive
-        return t
-    else:
-        return randword(f,length)
+    l=unicode(f.readline(),enc).split()
+    if len(l)>0:
+        t=l[randint(0,len(l)-1)]
+        t=t.strip().strip(punctuation)
+        if t!="" and isNoun(t) and len(t)>4: #numai substantive
+            return t
+    return randword(f,length,enc)
 
 def main():
     if len(sys.argv)<4:
-        print("Usage: python "+sys.argv[0]+" infile.txt outfile.txt nrofpairs")
+        print("Usage: python "+sys.argv[0]+" infile.txt outfile.txt nrofpairs [encoding]\nDefault encoding of infile is utf-8.")
         return
     #does not check duplicate pairs
+    if len(sys.argv)>=5:
+        enc=sys.argv[4]
+    else:
+        enc="utf-8"
     with open(sys.argv[1],"r")as f,open(sys.argv[2],"w+") as g:
         f.seek(0,2)
         length=f.tell()
         nrofpairs=int(sys.argv[3])
         i = nrofpairs
         while i!=0:
-            a,b=randword(f,length),randword(f,length)
+            a,b=randword(f,length,enc),randword(f,length,enc)
             if a!=b:
                 i-=1
                 g.write(a+" "+b+"\n")
